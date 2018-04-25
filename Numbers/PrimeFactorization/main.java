088ec9fa5c324d999339442b78eb58a8f2d8e26d
Sectioned <Sectioned@users.noreply.github.com>
2018-04-25 12:08:37 +0100

    ok

--- Numbers/PrimeFactorization/main.java
+++ Numbers/PrimeFactorization/main.java
@@ -12,7 +12,9 @@ Have the user enter a number and find all Prime Factors (if there are any) and d
 public class main{
 
 	public static Scanner scanner = new Scanner(System.in);
-	public static List<Integer> primeNumbers = new ArrayList<Integer>(Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199));
+	public static List<Integer> primeNumbers = new ArrayList<Integer>(Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 
+																					89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 
+																					181, 191, 193, 197, 199));
 
 	public static void main(String[] args){
 		System.out.print("Enter the number you would like the factors of: ");
